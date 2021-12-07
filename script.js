d7e6ec8f6ef12f1bfe1f44e171d52bcb97a293c5
alvjeff <alvjeff@hotmail.com>
2021-12-07 17:38:45 -0400

    difinicao de direcoes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = 'right'
+let direction = 'right';
 
 function criarBG(){
     context.fillStyle = 'lightgreen'; //funciona como o background
@@ -18,13 +18,27 @@ function criarBG(){
 //a cobrinha vai ser delocar da seguinte maneira: acrescentando um elemento a frente e retirando o ultimo
 function criarCobrinha(){
     //for que vai percorrer todo o array
-    for( let i = 0; i < snake; i++){
-        context.fillStyle ='blue';
+    for( i = 0; i < snake; i++){
+        context.fillStyle ='green';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+document.addEventListener('keydown', update)
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left'
+    if(event.keyCode == 38 && direction != 'down') direction = 'up'
+    if(event.keyCode == 39 && direction != 'left') direction = 'rigth'
+    if(event.keyCode == 40 && direction != 'up') direction = 'down'
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == 'right') snake[0].x = 0
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16*box
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16*box
+
     criarBG();
     criarCobrinha();
 
