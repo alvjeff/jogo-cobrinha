c7561be7babf49b619e3a99fa3831293f74cf448
alvjeff <alvjeff@hotmail.com>
2021-12-07 17:11:47 -0400

    difinicao de movimentos

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let box = 32
 
 let snake = [];
 snake[0] = {
-    x: 8*box,
-    y: 8*box
+    x: 8 * box,
+    y: 8 * box
 }
+let direction = 'right'
+
 function criarBG(){
     context.fillStyle = 'lightgreen'; //funciona como o background
     context.fillRect(0, 0, 16*box, 16*box);
@@ -17,11 +19,31 @@ function criarBG(){
 function criarCobrinha(){
     //for que vai percorrer todo o array
     for( let i = 0; i < snake; i++){
-        context.fillStyle = "green";
+        context.fillStyle ='blue';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
 
-criarBG();
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
 
+    if(direction == 'right') snakeX += box //acrescentar box para o lado
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box
+    if(direction == 'down') snakeY += box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
