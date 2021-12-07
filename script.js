77c9058b3f5b10691fdf72a03f2eae177f71f2a5
alvjeff <alvjeff@hotmail.com>
2021-12-07 16:49:10 -0400

    cobrinha criada

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d') //renderiza o desenho que vai acontecer dentro do campo
 let box = 32
 
+let snake = [];
+snake[0] = {
+    x: 8*box,
+    y: 8*box
+}
 function criarBG(){
     context.fillStyle = 'lightgreen'; //funciona como o background
-    context.fillRect(0, 0, 16*box, 16*box)
+    context.fillRect(0, 0, 16*box, 16*box);
 }
 
-criarBG()
+
+//a cobrinha vai ser delocar da seguinte maneira: acrescentando um elemento a frente e retirando o ultimo
+function criarCobrinha(){
+    //for que vai percorrer todo o array
+    for( let i = 0; i < snake; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+
+criarBG();
+
